12c13a31f02f761f12eb1510bcfbb690f2ad6af9
Tatsiana <mokosiejtatsiana@gmail.com>
2025-04-02 20:00:33 +0200

    Initial commit

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,23 +2,29 @@ package tests;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInfo;
 import org.openqa.selenium.WebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import pages.ApplicationManager;
 
 public class TestBase {
+
+    protected static ApplicationManager  app = new ApplicationManager(System.getProperty("browser", "chrome"));
     public WebDriver driver;
-    protected ApplicationManager app;
-    protected String browser;
+
+    public static Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     @BeforeEach
-    public void init() {
-        browser = System.getProperty("browser", "chrome");
-        app = new ApplicationManager(browser);
+    public void init(TestInfo testInfo) {
         driver = app.startTest();
+        logger.info("Start test ->" + testInfo.getTestMethod().toString());
     }
 
     @AfterEach
     public void tearDown() {
         app.stopTest();
+        logger.info("Stop test");
+        logger.info("*********************************");
     }
 }
