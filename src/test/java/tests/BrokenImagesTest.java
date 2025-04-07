4502e53a19b7061f9e4b8647d4f416d716d1abb1
Mokosiej <123974721+Mokosiej@users.noreply.github.com>
2025-04-07 19:42:30 +0200

    Update BrokenImagesTest.java

--- src/test/java/tests/BrokenImagesTest.java
+++ src/test/java/tests/BrokenImagesTest.java
@@ -21,6 +21,7 @@ public class BrokenImagesTest extends TestBase {
 
     @Test
     @DisplayName("Check for broken images")
+    @Tag("smoke")
     public void testBrokenImages() {
         int brokenCount = brokenImagesPage.getBrokenImagesCount();
         assertTrue(brokenCount > 0, "На странице нет сломанных изображений, что неожиданно");
